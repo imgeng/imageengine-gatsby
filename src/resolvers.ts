01fdbc961026e5f5a5120a6b842a7c9664d9d357
Rohan Raina <rraina@loginsoft.com>
2024-01-16 09:49:07 +0530

    added support for maxdpr

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -239,7 +239,7 @@ const gatsby_args = {
     },
     formats: {
         type: "[GatsbyImageFormat]",
-        description: "The image formats to generate. Valid values are AUTO (meaning the same format as the source image), JPG, PNG, WEBP and AVIF. The default value is [AUTO, WEBP], and you should rarely need to change this. Take care if you specify JPG or PNG when you do not know the formats of the source images, as this could lead to unwanted results such as converting JPEGs to PNGs. Specifying both PNG and JPG is not supported and will be ignored.",
+        description: "The image formats to generate. Valid values are AUTO (meaning the same format as the source image), JPG, PNG, WEBP AVIF and JXL. The default value is [AUTO, WEBP], and you should rarely need to change this. Take care if you specify JPG or PNG when you do not know the formats of the source images, as this could lead to unwanted results such as converting JPEGs to PNGs. Specifying both PNG and JPG is not supported and will be ignored.",
         defaultValue: [""],
     },
     breakpoints: {
